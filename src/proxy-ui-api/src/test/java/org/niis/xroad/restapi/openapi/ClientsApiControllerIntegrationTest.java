4a25cb976bbd3a094ecfd6d2109b58c7f520f707
Janne Mattila <janne.mattila@gofore.com>
2019-02-28 20:23:14 +0200

    functional runtime when deployed to SS. db password needs to be copied to application.properties manually

--- src/proxy-ui-api/src/test/java/org/niis/xroad/restapi/openapi/ClientsApiControllerIntegrationTest.java
+++ src/proxy-ui-api/src/test/java/org/niis/xroad/restapi/openapi/ClientsApiControllerIntegrationTest.java
@@ -1,3 +1,27 @@
+/**
+ * The MIT License
+ * Copyright (c) 2018 Estonian Information System Authority (RIA),
+ * Nordic Institute for Interoperability Solutions (NIIS), Population Register Centre (VRK)
+ * Copyright (c) 2015-2017 Estonian Information System Authority (RIA), Population Register Centre (VRK)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package org.niis.xroad.restapi.openapi;
 
 import org.junit.Before;
@@ -21,6 +45,9 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+/**
+ * fsdfsd
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureTestDatabase
@@ -53,7 +80,8 @@ public class ClientsApiControllerIntegrationTest {
     @Test
     @WithMockUser(roles = "XROAD_REGISTRATION_OFFICER")
     public void getClients() {
-        ResponseEntity<List<org.niis.xroad.restapi.openapi.model.Client>> response = clientsApiController.getClients();
+        ResponseEntity<List<org.niis.xroad.restapi.openapi.model.Client>> response =
+                clientsApiController.getClients();
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals(2, response.getBody().size());
         org.niis.xroad.restapi.openapi.model.Client client = response.getBody().get(0);
@@ -65,9 +93,10 @@ public class ClientsApiControllerIntegrationTest {
     @WithMockUser(roles = "WRONG_ROLE")
     public void forbidden() {
         try {
-            ResponseEntity<List<org.niis.xroad.restapi.openapi.model.Client>> response = clientsApiController.getClients();
+            ResponseEntity<List<org.niis.xroad.restapi.openapi.model.Client>> response =
+                    clientsApiController.getClients();
             fail("should throw AccessDeniedException");
-        } catch (AccessDeniedException expected) {}
+        } catch (AccessDeniedException expected) { }
     }
 
 }
