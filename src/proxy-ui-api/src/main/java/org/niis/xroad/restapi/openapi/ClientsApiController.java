230ee1c97372dd4ce3216f2e3c0ea72743282038
Janne Mattila <janne.mattila@gofore.com>
2019-03-01 17:01:01 +0200

    revert to transactional controller

--- src/proxy-ui-api/src/main/java/org/niis/xroad/restapi/openapi/ClientsApiController.java
+++ src/proxy-ui-api/src/main/java/org/niis/xroad/restapi/openapi/ClientsApiController.java
@@ -36,6 +36,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -55,7 +56,7 @@ import java.util.Optional;
 @RequestMapping("/api")
 @Slf4j
 @DenyAll
-//@Transactional
+@Transactional
 public class ClientsApiController implements org.niis.xroad.restapi.openapi.ClientsApi {
 
     private final NativeWebRequest request;
